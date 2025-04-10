ac876d2e3db92cd516891499f4c7ac1919584e8a
Aure655 <aurelynmarievargasroman@gmail.com>
2025-04-10 18:27:22 -0400

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,10 @@
+
+function showRegister() {
+    document.getElementById('login-box').classList.remove('active');
+    document.getElementById('register-box').classList.add('active');
+}
+
+function showLogin() {
+    document.getElementById('register-box').classList.remove('active');
+    document.getElementById('login-box').classList.add('active');
+}
